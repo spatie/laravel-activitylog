eeb5650042e621489dcaa855fcd083691e0fc779
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-27 08:16:51 +0200

    Whitespace normalization

--- src/ActivityLogger.php
+++ src/ActivityLogger.php
@@ -53,7 +53,7 @@ class ActivityLogger
 
         return $this;
     }
-    
+
     public function by($modelOrId)
     {
         return $this->causedBy($modelOrId);
@@ -87,9 +87,9 @@ class ActivityLogger
 
     /**
      * @param \Illuminate\Database\Eloquent\Model|int|string $modelOrId
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Model
-     * 
+     *
      * @throws \Spatie\Activitylog\Exceptions\CouldNotLogActivity
      */
     protected function normalizeCauser($modelOrId): Model
