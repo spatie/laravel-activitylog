30fcd5d131c2c15a2edfd2ca55f1827e250970e5
freek <freek@spatie.be>
2016-06-27 14:21:16 +0200

    show test names

--- tests/ActivityloggerTest.php
+++ tests/ActivityloggerTest.php
@@ -140,7 +140,11 @@ class ActivityloggerTest extends TestCase
         $this->assertEquals($userId, $this->getLastActivity()->causer->id);
     }
 
-
+    /** 
+     * @test
+     * 
+     * @requires !Travis
+     */
     public function it_can_replace_the_placeholders()
     {
         $article = Article::create(['name' => 'article name']);
@@ -158,7 +162,11 @@ class ActivityloggerTest extends TestCase
         $this->assertEquals($expectedDescription, $this->getLastActivity()->description);
     }
 
-
+    /**
+     * @test
+     *
+     * @requires !Travis
+     * */
     public function it_will_not_replace_non_placeholders()
     {
         $description = 'hello: :hello';
