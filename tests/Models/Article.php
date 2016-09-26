6f72283dd2cee6feb1584615f8a8d9bc04ae9ba2
xcaptain <joey.xf@gmail.com>
2016-09-26 10:57:38 +0800

    delete event log fail on real delete
    soft delete works fine but real delete doesn't

--- tests/Models/Article.php
+++ tests/Models/Article.php
@@ -3,12 +3,9 @@
 namespace Spatie\Activitylog\Test\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'articles';
 
     protected $guarded = [];
