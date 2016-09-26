b47988ca11fdd5bcbbede9e8dfb70c6548da1d5b
a3020 <a3020@users.noreply.github.com>
2016-09-26 20:14:46 +0200

    Minor tweaks to config comments

--- config/laravel-activitylog.php
+++ config/laravel-activitylog.php
@@ -3,30 +3,30 @@
 return [
 
     /*
-     * When set to false, no activities will be saved to database.
+     * If set to false, no activities will be saved to the database.
      */
     'enabled' => env('ACTIVITY_LOGGER_ENABLED', true),
 
     /*
-     * When running the clean-command all recording activities older than
+     * When the clean-command is executed, all recording activities older than
      * the number of days specified here will be deleted.
      */
     'delete_records_older_than_days' => 365,
 
     /*
-     * When not specifying a log name when logging activity
-     * we'll using this log name.
+     * If no log name is passed to the activity() helper
+     * we use this default log name.
      */
     'default_log_name' => 'default',
 
     /*
      * You can specify an auth driver here that gets user models.
-     * When this is null we'll use the default Laravel auth driver.
+     * If this is null we'll use the default Laravel auth driver.
      */
     'default_auth_driver' => null,
 
     /*
-     * When set to true, the subject returns soft deleted models.
+     * If set to true, the subject returns soft deleted models.
      */
      'subject_returns_soft_deleted_models' => false,
 
