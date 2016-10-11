3e7e0896d3174274210de5c463207b617b12a2d8
Peter Matseykanets <pmatseykanets@users.noreply.github.com>
2016-10-11 17:52:59 -0400

    Fix scopeForSubject() in Activity model #89. (#91)
    * Fix scopeForSubject() in Activity model #89.
    
    * Improve test for scopeForSubject().

--- tests/ActivityModelTest.php
+++ tests/ActivityModelTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\Activitylog\Test;
 
 use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Test\Models\Article;
 use Spatie\Activitylog\Test\Models\User;
 
 class ActivityModelTest extends TestCase
@@ -61,9 +62,19 @@ class ActivityModelTest extends TestCase
     /** @test */
     public function it_provides_a_scope_to_get_log_items_for_a_specific_subject()
     {
-        $subject = User::first();
-        $activity = Activity::forSubject($subject)->get();
+        $subject = Article::first();
+        $causer = User::first();
 
-        $this->assertCount($subject->activity->count(), $activity);
+        activity()->on($subject)->by($causer)->log('Foo');
+        activity()->on(Article::create([
+            'name' => 'Another article',
+        ]))->by($causer)->log('Bar');
+
+        $activities = Activity::forSubject($subject)->get();
+
+        $this->assertCount(1, $activities);
+        $this->assertEquals($subject->getKey(), $activities->first()->subject_id);
+        $this->assertEquals(get_class($subject), $activities->first()->subject_type);
+        $this->assertEquals('Foo', $activities->first()->description);
     }
 }
