4e8471016574d29015acd64191e3cd4abbebd2b9
belousov.a <belousov.a@uppay.me>
2017-06-14 18:44:15 +0300

    The StyleCI

--- src/Traits/DetectsChanges.php
+++ src/Traits/DetectsChanges.php
@@ -94,19 +94,20 @@ trait DetectsChanges
     }
 
 
-    public function array_diff_assoc_recursive($array1, $array2) {
-        $difference = array();
+    private function array_diff_assoc_recursive($array1, $array2): array
+    {
+        $difference = [];
         foreach ($array1 as $key => $value) {
             if (is_array($value)) {
-                if (!array_key_exists($key, $array2) || !is_array($array2[$key])) {
+                if (! array_key_exists($key, $array2) || ! is_array($array2[$key])) {
                     $difference[$key] = $value;
                 } else {
                     $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
-                    if (!empty($new_diff)) {
+                    if (! empty($new_diff)) {
                         $difference[$key] = $new_diff;
                     }
                 }
-            } elseif (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
+            } elseif (! array_key_exists($key, $array2) || $array2[$key] !== $value) {
                 $difference[$key] = $value;
             }
         }
