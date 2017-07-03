c6da1ce3502b6a6b69311dc4a325b389c6d710ed
Uriah Galang <g0ld3lux@gmail.com>
2017-07-03 09:46:41 +0800

    added OtherUserGuard Model on TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Spatie\Activitylog\Test;
 
 use Spatie\Activitylog\Models\Activity;
 use Spatie\Activitylog\Test\Models\User;
+use Spatie\Activitylog\Test\Models\OtherGuardUser;
 use Illuminate\Database\Schema\Blueprint;
 use Spatie\Activitylog\Test\Models\Article;
 use Spatie\Activitylog\ActivitylogServiceProvider;
