4781e69bad90f2cd2f298e4d5dd587299c17e84d
Kevin Ruscoe <kevdotbadger@gmail.com>
2017-10-16 21:26:48 +0100

    How about now ;)

--- src/ActivityLogger.php
+++ src/ActivityLogger.php
@@ -101,7 +101,7 @@ class ActivityLogger
 
     /**
      * Sets the model this activity was performed on.
-     * 
+     *
      * @param \Illuminate\Database\Eloquent\Model
      * @return $this
      */
