512476e489125808e9cf3a93630ec6c9734b5842
ricardoaugusto <oi@ricardoaugusto.com>
2017-11-07 12:02:53 -0200

    Adds a log_name argument to activitylog:clean command (#301)
    * Adds a log_name argument to activitylog:clean command
    
    Arguments:
    log_name (optional) The log name that will be cleaned. [default: "default"]
    
    * Renames argument "log_name" to simply "log"
    
    * Added condition when $log is null
    
    * StyleCI validation
    
    * CleanActivitylogCommandTest now creates Activity with log_name = default
    
    * Reverting to scope
    
    * Reverting back to query conditional
    
    * Changes signature + CleanActivitylogCommandTest restore

--- src/CleanActivitylogCommand.php
+++ src/CleanActivitylogCommand.php
@@ -12,7 +12,8 @@ class CleanActivitylogCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'activitylog:clean';
+    protected $signature = 'activitylog:clean
+                            {log? : (optional) The log name that will be cleaned.}';
 
     /**
      * The console command description.
@@ -25,13 +26,19 @@ class CleanActivitylogCommand extends Command
     {
         $this->comment('Cleaning activity log...');
 
+        $log = $this->argument('log');
+
         $maxAgeInDays = config('activitylog.delete_records_older_than_days');
 
         $cutOffDate = Carbon::now()->subDays($maxAgeInDays)->format('Y-m-d H:i:s');
 
         $activity = ActivitylogServiceProvider::getActivityModelInstance();
 
-        $amountDeleted = $activity::where('created_at', '<', $cutOffDate)->delete();
+        $amountDeleted = $activity::where('created_at', '<', $cutOffDate)
+            ->when($log !== null, function ($query) use ($log) {
+                $query->inLog($log);
+            })
+            ->delete();
 
         $this->info("Deleted {$amountDeleted} record(s) from the activity log.");
 
