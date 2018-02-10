defe82bcbaf7e63647ffcb82581f275e16cb35ee
Alex Lisenkov <alex@specialistinwebsites.nl>
2018-02-10 16:51:53 +0100

    Use child class instead of base class

--- src/Traits/LogsActivity.php
+++ src/Traits/LogsActivity.php
@@ -88,7 +88,7 @@ trait LogsActivity
             'deleted',
         ]);
 
-        if (collect(class_uses_recursive(__CLASS__))->contains(SoftDeletes::class)) {
+        if (collect(class_uses_recursive(static::class))->contains(SoftDeletes::class)) {
             $events->push('restored');
         }
 
