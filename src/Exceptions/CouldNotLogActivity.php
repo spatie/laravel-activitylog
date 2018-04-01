5b79ae6802139d5abe52fc9f724abfeccbab21dd
Mohamed Gamal <mg.freelancer92@gmail.com>
2018-04-01 23:39:25 +0200

    new self VS new static

--- src/Exceptions/CouldNotLogActivity.php
+++ src/Exceptions/CouldNotLogActivity.php
@@ -8,6 +8,9 @@ class CouldNotLogActivity extends Exception
 {
     public static function couldNotDetermineUser($id)
     {
+    	// [new self] refers to the same class in which the new keyword is actually written.
+
+		// [new static], in PHP 5.3's late static bindings, refers to whatever class in the hierarchy you called the method on.
         return new static("Could not determine a user with identifier `{$id}`.");
     }
 }
