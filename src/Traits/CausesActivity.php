2725cc9154b0c41c757f93ae350e99bf3bd4b955
Tom Witkowski <dev.gummibeer@gmail.com>
2018-10-16 00:20:36 +0200

    Rename relations, remove duplicates and drop deprecated (#439)
    * drop deprecated method
    
    * replace duplicating trait/method
    
    * rename method to plural
    
    * fix phpunit tests
    
    * move unittest into related test class
    
    * fix php cs

--- src/Traits/CausesActivity.php
+++ src/Traits/CausesActivity.php
@@ -7,14 +7,8 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait CausesActivity
 {
-    public function activity(): MorphMany
+    public function actions(): MorphMany
     {
         return $this->morphMany(ActivitylogServiceProvider::determineActivityModel(), 'causer');
-    }
-
-    /** @deprecated Use activity() instead */
-    public function loggedActivity(): MorphMany
-    {
-        return $this->activity();
     }
 }
