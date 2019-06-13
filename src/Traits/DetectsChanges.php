e005c339f685f0251b338c1061a24b76706ddfc1
Rias <hello@rias.be>
2019-06-13 09:25:44 +0000

    Apply fixes from StyleCI

--- src/Traits/DetectsChanges.php
+++ src/Traits/DetectsChanges.php
@@ -111,7 +111,7 @@ trait DetectsChanges
             throw CouldNotLogChanges::invalidAttribute($attribute);
         }
 
-        list($relatedModelName, $relatedAttribute) = explode('.', $attribute);
+        [$relatedModelName, $relatedAttribute] = explode('.', $attribute);
 
         $relatedModel = $model->$relatedModelName ?? $model->$relatedModelName();
 
