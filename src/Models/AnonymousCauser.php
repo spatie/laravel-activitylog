bc4d1361013565acedf6c539139ed3d492c68d89
John Braun <johnbraun@pm.me>
2019-10-02 22:04:22 +0200

    Add model/migration for Anonymous Causers
    Add an AnonymousCauser "null object" (model + migration) for a causer of activity.

---
+++ src/Models/AnonymousCauser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Spatie\Activitylog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnonymousCauser extends Model
+{
+    public $guarded = [];
+
+    public function __construct(array $attributes = [])
+    {
+        if (! isset($this->connection)) {
+            $this->setConnection(config('activitylog.database_connection'));
+        }
+
+        if (! isset($this->table)) {
+            $this->setTable(config('activitylog.anonymous_causers_table_name'));
+        }
+
+        parent::__construct($attributes);
+    }
+}
