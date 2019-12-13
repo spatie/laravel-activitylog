d9e67eadda3792fa3c774c7e1237cda6c137f6f7
EthicalJobsJesse <56899649+EthicalJobsJesse@users.noreply.github.com>
2019-12-13 15:17:14 +1100

    Config to allow return soft-deleted causers

--- config/activitylog.php
+++ config/activitylog.php
@@ -29,6 +29,11 @@ return [
      * If set to true, the subject returns soft deleted models.
      */
     'subject_returns_soft_deleted_models' => false,
+    
+    /*
+     * If set to true, the subject returns soft deleted models.
+     */
+    'causer_returns_soft_deleted_models' => false,
 
     /*
      * This model will be used to log activity.
