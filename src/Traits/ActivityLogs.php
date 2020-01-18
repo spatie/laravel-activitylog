157fc1bf860dae8420c26293e85d253d8da4b570
Musa <40173603+voyula@users.noreply.github.com>
2020-01-18 14:34:58 +0300

    Update ActivityLogs.php

--- src/Traits/ActivityLogs.php
+++ src/Traits/ActivityLogs.php
@@ -2,13 +2,13 @@
 
 namespace Spatie\Activitylog\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Spatie\Activitylog\ActivityLogger;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Activitylog\ActivitylogServiceProvider;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait ActivityLogs
 {
