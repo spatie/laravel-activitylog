dfcd413ce239289a88bc4b9f21f61bd948a01c6f
Tom Witkowski <dev.gummibeer@gmail.com>
2020-02-22 18:18:35 +0000

    Apply fixes from StyleCI

--- src/ActivityLogger.php
+++ src/ActivityLogger.php
@@ -2,11 +2,11 @@
 
 namespace Spatie\Activitylog;
 
-use Illuminate\Support\Carbon;
 use Illuminate\Auth\AuthManager;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Traits\Macroable;
 use Spatie\Activitylog\Contracts\Activity as ActivityContract;
 use Spatie\Activitylog\Exceptions\CouldNotLogActivity;
