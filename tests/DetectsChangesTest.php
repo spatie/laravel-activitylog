1cd68db2bc7bea3b3e4ae285ebb6afd376d2c6f3
Tom Witkowski <dev.gummibeer@gmail.com>
2021-04-27 16:59:05 +0200

    fix interval testcase

--- tests/DetectsChangesTest.php
+++ tests/DetectsChangesTest.php
@@ -138,8 +138,7 @@ class DetectsChangesTest extends TestCase
             public function getActivitylogOptions() : LogOptions
             {
                 return LogOptions::defaults()
-                ->logAll()
-                ->dontLogIfAttributesChangedOnly(['created_at', 'updated_at', 'deleted_at'])
+                ->logOnly(['name', 'interval'])
                 ->logOnlyDirty();
             }
         };
