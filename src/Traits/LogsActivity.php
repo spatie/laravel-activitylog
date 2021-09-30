1505bd80dfb8c3e55ff31c1e72a4b503268b25a2
mo7zayed <mohamedzayed907@gmail.com>
2021-09-30 15:14:45 +0200

    Fix case if the model is using SoftDeletes trait

--- src/Traits/LogsActivity.php
+++ src/Traits/LogsActivity.php
@@ -171,8 +171,10 @@ trait LogsActivity
             return true;
         }
 
-        if (Arr::has($this->getDirty(), $this->getDeletedAtColumn())) {
-            if ($this->getDirty()[$this->getDeletedAtColumn()] === null) {
+        $deletedAtColumn = method_exists($this, 'getDeletedAtColumn') ? $this->getDeletedAtColumn() : 'deleted_at';
+
+        if (Arr::has($this->getDirty(), $deletedAtColumn)) {
+            if ($this->getDirty()[$deletedAtColumn] === null) {
                 return false;
             }
         }
