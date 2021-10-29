0b8039f2bafb8e790b62092473699c4945780318
Steve Bauman <6421846+stevebauman@users.noreply.github.com>
2021-10-29 16:54:41 -0400

    Submit failing test

---
+++ tests/Models/ArticleWithLogDescriptionClosure.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Spatie\Activitylog\Test\Models;
+
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class ArticleWithLogDescriptionClosure extends Article
+{
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->setDescriptionForEvent(function ($eventName) {
+                return "$eventName";
+            });
+    }
+}
