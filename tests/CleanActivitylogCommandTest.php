742a43807cd6d1486f46ad5a9792867a50fbc6b6
philippe <philippe@code16.fr>
2023-02-02 14:08:39 +0100

    Impact tests

--- tests/CleanActivitylogCommandTest.php
+++ tests/CleanActivitylogCommandTest.php
@@ -1,7 +1,8 @@
 <?php
 
 use Carbon\Carbon;
-use Spatie\Activitylog\Models\Activity;
+use Spatie\Activitylog\Contracts\Activity;
+use Spatie\Activitylog\Traits\AllowsMassPruning;
 
 beforeEach(function () {
     Carbon::setTestNow(Carbon::create(2016, 1, 1, 00, 00, 00));
@@ -9,40 +10,50 @@ beforeEach(function () {
     app()['config']->set('activitylog.delete_records_older_than_days', 31);
 });
 
-it('can clean the activity log', function () {
-    collect(range(1, 60))->each(function (int $index) {
-        Activity::create([
+it('can clean the activity log', function (Activity $activityImpl) {
+    app()['config']->set('activitylog.activity_model', get_class($activityImpl));
+
+    collect(range(1, 60))->each(function (int $index) use ($activityImpl) {
+        $activityImpl::create([
             'description' => "item {$index}",
             'created_at' => Carbon::now()->subDays($index)->startOfDay(),
         ]);
     });
 
-    expect(Activity::all())->toHaveCount(60);
+    expect($activityImpl::all())->toHaveCount(60);
 
     Artisan::call('activitylog:clean');
 
-    expect(Activity::all())->toHaveCount(31);
+    expect($activityImpl::all())->toHaveCount(31);
 
     $cutOffDate = Carbon::now()->subDays(31)->format('Y-m-d H:i:s');
 
-    expect(Activity::where('created_at', '<', $cutOffDate)->get())->toHaveCount(0);
-});
+    expect($activityImpl::where('created_at', '<', $cutOffDate)->get())->toHaveCount(0);
+})->with([
+    'mass pruning' => fn() => new Spatie\Activitylog\Models\Activity,
+    'regular deletion' => fn() => new Spatie\Activitylog\Test\Models\Activity,
+]);
 
-it('can accept days as option to override config setting', function () {
-    collect(range(1, 60))->each(function (int $index) {
-        Activity::create([
+it('can accept days as option to override config setting', function (Activity $activityImpl) {
+    app()['config']->set('activitylog.activity_model', get_class($activityImpl));
+
+    collect(range(1, 60))->each(function (int $index) use ($activityImpl) {
+        $activityImpl::create([
             'description' => "item {$index}",
             'created_at' => Carbon::now()->subDays($index)->startOfDay(),
         ]);
     });
 
-    expect(Activity::all())->toHaveCount(60);
+    expect($activityImpl::all())->toHaveCount(60);
 
     Artisan::call('activitylog:clean', ['--days' => 7]);
 
-    expect(Activity::all())->toHaveCount(7);
+    expect($activityImpl::all())->toHaveCount(7);
 
     $cutOffDate = Carbon::now()->subDays(7)->format('Y-m-d H:i:s');
 
-    expect(Activity::where('created_at', '<', $cutOffDate)->get())->toHaveCount(0);
-});
+    expect($activityImpl::where('created_at', '<', $cutOffDate)->get())->toHaveCount(0);
+})->with([
+    'mass pruning' => fn() => new Spatie\Activitylog\Models\Activity,
+    'regular deletion' => fn() => new Spatie\Activitylog\Test\Models\Activity,
+]);
