6221f423acde425f2a57c66418790580932d1471
Diego Relyveld <me@diegor.nl>
2023-10-29 14:18:00 +0100

    Make Activity model $subject PHPDoc nullable

--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -26,7 +26,7 @@ use Spatie\Activitylog\Contracts\Activity as ActivityContract;
  * @property \Carbon\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent|null $causer
  * @property-read \Illuminate\Support\Collection $changes
- * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $subject
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent|null $subject
  *
  * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Activitylog\Models\Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
  * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Activitylog\Models\Activity forBatch(string $batchUuid)
